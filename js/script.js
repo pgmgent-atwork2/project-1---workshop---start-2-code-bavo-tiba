bcf24917dd35b4b7d1274e7e9ef4e46dd6587e11
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2023-04-18 17:40:30 +0200

    added comments and formatted file

--- js/script.js
+++ js/script.js
@@ -1,43 +1,57 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
 
 let clickedCard = false;
 let firstCard, secondCard;
 
-cards.forEach(card => card.addEventListener('click', showCard));
+cards.forEach((card) => card.addEventListener("click", showCard));
 
 function showCard() {
-    this.classList.toggle('show');
+  // when card is clicked show class is added or removed
+  this.classList.toggle("show");
 
-    console.log(this.getAttribute('data-color'))
+  console.log(this.getAttribute("data-color"));
 
-    if (!clickedCard) {
-      clickedCard = true;
-      firstCard = this;
-      return;
-    }
+  if (!clickedCard) {
+    // if its the first card that is clicked save it to firstcard
+    clickedCard = true;
+    firstCard = this;
+    return;
+  }
+  // if its the second card save it to secondcard
+  secondCard = this;
+  clickedCard = false;
 
-    secondCard = this;
-    clickedCard = false;
-
+  // if both cards clicked look for match
   checkCards();
-};
-
+}
 
 function checkCards() {
-  if (firstCard.getAttribute('data-color') === secondCard.getAttribute('data-color')) {
+  // check if both cards are same color if not reset cards
+  if (
+    firstCard.getAttribute("data-color") ===
+    secondCard.getAttribute("data-color")
+  ) {
     disableCards();
   }
   resetCards();
-};
+}
 
 function disableCards() {
-  firstCard.removeEventListener('click', showCard);
-  secondCard.removeEventListener('click', showCard);
-};
+  // if both correct cards are clicked disable eventListener
+  firstCard.removeEventListener("click", showCard);
+  secondCard.removeEventListener("click", showCard);
+}
 
 function resetCards() {
+  // remove show from cards after small timeout
   setTimeout(() => {
-    firstCard.classList.toggle('show');
-    secondCard.classList.toggle('show');
+    firstCard.classList.toggle("show");
+    secondCard.classList.toggle("show");
   }, 1500);
-};
+}
+
+// todo ability to click more than two cards
+
+// clicking same card twice
+
+// make the cards random
