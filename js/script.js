c4d0a5b70ef3211b8f28a76db618c6f3d6c4b07d
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2023-04-20 09:50:47 +0200

    added ability to lock the board

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@ const cards = document.querySelectorAll(".card");
 
 let clickedCard = false;
 let firstCard, secondCard;
+let lock = false;
 
 cards.forEach((card) => card.addEventListener("click", showCard));
 
 function showCard() {
+  // when there are 2 cards clicked return so no more cards can be clicked
+  if (lock) return;
   // when card is clicked show class is added or removed
   this.classList.toggle("show");
-
+  console.log(lock);
   console.log(this.getAttribute("data-color"));
 
   if (!clickedCard) {
@@ -40,22 +43,25 @@ function disableCards() {
   // if both correct cards are clicked disable eventListener
   firstCard.removeEventListener("click", showCard);
   secondCard.removeEventListener("click", showCard);
+  firstCard.classList.add("done");
+  secondCard.classList.add("done");
 }
 
 function resetCards() {
+  // lock the board when 2 cards are clicked
+  lock = true;
   // remove show from cards after small timeout
   setTimeout(() => {
-    firstCard.classList.toggle("show");
-    secondCard.classList.toggle("show");
+    firstCard.classList.remove("show");
+    secondCard.classList.remove("show");
+    
+    // unlock the board after timeout
+    lock = false;
   }, 1500);
 }
 
-// todo ability to click more than two cards
+// todo remove ability to click more than two cards
 
 // clicking same card twice
 
-<<<<<<< HEAD
 // make the cards random
-=======
-// make the cards random
->>>>>>> b92e6d0d3001110c8392c0bc6e17943aa09041dc
